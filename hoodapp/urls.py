a3075b115c49f8eae6a03c04c44111198d0bc9d0
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-19 17:56:55 +0300

    adds routes to add_hood,edit_hood pages

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-  url('^$',views.home,name = 'home'),
+  url('^$',views.index,name = 'index'),
   url(r'^signup/$', views.signup, name='signup'),
   url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
       views.activate, name='activate'),
-  url(r'^search/',views.search_business, name='search_business')
+  url(r'^search/',views.search_business, name='search_business'),
+  url(r'^add_hood/$', views.add_hood, name='add_hood'),
+  url(r'^edit_hood/(\d+)',views.edit_hood,name="edit_hood"),
 ]
 
 if settings.DEBUG:
