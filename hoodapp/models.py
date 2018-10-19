245c3edd26bb523d151ff58c93b333b3141e19f6
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-19 12:56:20 +0300

    creates model class properties,methods and functions

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,8 +9,26 @@ class Neighbourhood(models.Model):
   '''
   class that contains Neighbourhood properties
   '''
-  name = models.CharField(max_length=30,null=True,blank=True)
-  location = models.CharField(max_length=30,null=True,blank=True)
+  NEIGHBOURHOOD_CHOICES = (
+    ('South B','South B'),
+    ('South C','South C'),
+    ('Westlands','Westtlands'),
+    ('Donholm','Donholm'),
+    ('Ruaka','Ruaka'),
+    ('Imara Daima','Imara Daima'),
+    ('Syokimau','Syokimau'),
+    ('Buruburu','Buruburu'),
+    ('Kinoo','Kinoo'),
+    ('Komarock','Komarock'),
+    ('Madaraka','Madaraka'),
+    ('Rongai','Rongai'),
+    ('Karen','Karen'),
+    ('Ruiru','Ruiru'),
+    ('Roysambu','Roysambu'),
+    ('Juja','Juja'),
+  )
+  name = models.CharField(max_length=200,null=True,blank=True)
+  location = models.CharField(max_length=30,choices=NEIGHBOURHOOD_CHOICES)
   occupants_count = models.PositiveIntegerField()
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -35,21 +54,16 @@ class Neighbourhood(models.Model):
     neighbourhood = Neighbourhood.objects.get(neighbourhood_id=id)
     return neighbourhood
 
-  
-
-
-
-class Profile(models.Model):
-  '''
-  class that contains User properties
-  '''
-  name = models.CharField(max_length=30,null=True,blank=True)
-  email = models.EmailField()
-  neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE,blank=True,null=True)
-  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-
   def __str__(self):
     return self.name
+
+  
+class Profile(models.Model):
+  '''
+  class that contains user Profile properties
+  '''
+  bio = models.TextField()
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
 
   @receiver(post_save, sender=User)
   def create_user_profile(sender, instance, created, **kwargs):
@@ -62,25 +76,19 @@ class Profile(models.Model):
 
   post_save.connect(save_user_profile, sender=User)
 
-  @classmethod
-  def get_profile(cls):
-    profile = Profile.objects.all()
-    return profile
-
-  @classmethod
-  def get_profile_by_id(cls,id):
-    user_profile = Profile.objects.get(user=id)
-    return user_profile
+  def __str__(self):
+    return self.bio
 
 
 class Business(models.Model):
+  '''
+  Business class with Business properties,methods and function 
+  '''
   name = models.CharField(max_length=30)
+  description = models.TextField(default="",blank=True,null=True)
   email = models.EmailField()
   user = models.ForeignKey(User)
   neighbourhood = models.ForeignKey(Neighbourhood)
-
-  def __str__(self):
-    self.name
 
   def save_business(self):
     self.save
@@ -100,3 +108,51 @@ class Business(models.Model):
   def search_by_title(cls,search_term):
     business = cls.objects.filter(title__icontains=search_term)
     return business
+
+  def __str__(self):
+    self.name
+
+class JoinHood(models.Model):
+  '''
+  class that enables one join neighbourhoods
+  '''
+  user_id = models.OneToOneField(User)
+  neighbourhood_id = models.ForeignKey(Neighbourhood)
+
+  def __str__(self):
+    return self.user_id
+
+class Posts(models.Model):
+  '''
+  Class that enables one create a post on a neighbourhood
+  '''
+  topic = models.CharField(max_length=100)
+  post = models.TextField()
+  user = models.ForeignKey(User)
+  hood = models.ForeignKey(Neighbourhood)
+
+  def save_posts(self):
+    self.save()
+
+  def delete_posts(self):
+    self.delete()
+
+  def __str__(self):
+    return self.topic
+
+class Comments(models.Model):
+  '''
+  Class that enables commenting on a posted topic
+  '''
+  comment = models.TextField()
+  user = models.ForeignKey(User)
+  post = models.ForeignKey(Posts)
+
+  def save_comment(self):
+    self.save()
+
+  def delete_comment(self):
+    self.delete()
+
+  def __str__(self):
+    return self.comment
